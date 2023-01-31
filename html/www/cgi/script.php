c59956bcf0af8a8a6c2b6a607d27b3999cb98976
pboidin <pierrelouisboidin@gmail.com>
2023-01-31 22:11:21 +0100

    add html & css

---
+++ html/www/cgi/script.php
@@ -0,0 +1,27 @@
+<html>
+<head><?php header("test: abcdef"); ?><meta charset="utf-8"/></head>
+<body>
+
+
+
+    <?php
+
+
+    echo '<br/>';
+    echo '<br/>';
+
+    echo "POST : ";
+    print_r($_POST);
+    echo '<br/>';
+    echo '<br/>';
+
+    echo "GET : ";
+    print_r($_GET);
+
+
+    ?>
+
+</body>
+
+
+</html>
