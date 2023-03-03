9f78ac3af9097be12d61c9194e0259a7f1cf9d1e
pboidin <pierrelouisboidin@gmail.com>
2023-03-03 12:45:01 +0100

    add Cookies

--- html/www/post.php
+++ html/www/post.php
@@ -3,6 +3,21 @@
   header_remove('Content-Type');
 ?>
 
+<?php
+		if (isset($_POST['title']))
+		{
+			$cookie_name1 = "title";
+			$cookie_value1 = $_POST['title'];
+			setcookie($cookie_name1, $cookie_value1, time() + (86400 * 30), "/"); // 86400 = 1 day
+		}
+		if (isset($_POST['content']))
+		{
+			$cookie_name2 = "content";
+			$cookie_value2 = $_POST['content'];
+			setcookie($cookie_name2, $cookie_value2, time() + (86400 * 30), "/"); // 86400 = 1 day
+		}
+?>
+
 <!DOCTYPE html>
 
 <head>
@@ -11,8 +26,28 @@
 
 <h1>Hello this is post request ! Voici les infos que tu as rentrees :)</h1>
 <body style="text-align: center;">
+    <p> Voila ton mot <?php
+        if (isset($_POST["title"])) {
+            echo htmlspecialchars($_POST["title"]);
+        } else if (isset($COOKIE["title"])) {
+            echo htmlspecialchars($_COOKIE["title"]);
+        } else {
+            echo "Jules";
+        }
+        ?>
+    </p>
+    <p> et la description: <?php
+        if (isset($_POST["content"])) {
+            echo htmlspecialchars($_POST["content"]);
+        } else if (isset($COOKIE["content"])) {
+            echo htmlspecialchars($_COOKIE["content"]);
+        } else {
+            echo "Peyron";
+        }
+        ?>
+    </p>
     <?php if(isset($_GET["title"])) { ?>
-        <h3>voila ton mot:</h3> <p><?=$_GET["title"];?></p>
+        <h3>voila ton mot:</h3><p><?=$_GET["title"];?></p>
     <?php } ?>
     <?php if(isset($_GET["content"])) { ?>
         <h3>et la description :</h3> <p><?=$_GET["content"];?></p>
